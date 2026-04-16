a1a532e2bbc76819e63e730807f0ae5b508fdf54
Terrence <terrencemaina77@gmail.com>
2026-04-16 19:35:20 +0300

    Next step fixing STK push

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // IMPORTANT: you'll use a service account key (next step)
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
